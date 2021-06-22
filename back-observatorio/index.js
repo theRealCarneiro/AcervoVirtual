bdda7262581c1f153fadc9ac4388e2b1aa90fc46
lucasestevaos <lucas.estevao@hotmail.com.br>
2021-06-22 13:40:35 -0300

    mudança imagem cabecalho

--- back-observatorio/index.js
+++ back-observatorio/index.js
@@ -1,4 +1,5 @@
 const customExpress = require('./config/customExpress')
+
 const conexao = require('./infraestrutura/conexao')
 const Tabelas = require('./infraestrutura/tabelas')
 
--- back-observatorio/infraestrutura/conexao.js
+++ back-observatorio/infraestrutura/conexao.js
@@ -6,7 +6,7 @@ const conexao = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
-    password: '!@#QWEASD123qweasd',
+    password: '1qaz2wsx##E',
     database: 'acervo'
 })
 
