cf97ce0561dd1a5d9bcc47e97cde5c6721a65416
theRealCarneiro <gabriel.chaves.carneiro@gmail.com>
2021-08-08 09:27:22 -0300

    changed jsondb script to be executed by #!/usr/bin/env node

--- backend/middleware/jsondb.js
+++ backend/middleware/jsondb.js
@@ -1,14 +1,18 @@
-const result = require('dotenv').config({path:'/var/www/src/observatorio/backend/.env'});
-const acervo = require('../models/acervo');
-const fs = require('fs');
+#!/usr/bin/env node
 
 //const jsonPath = ('./bd.json');
 //const jsonPath = '/var/www/src/observatorio/frontend/src/assets/bd/bd.json'
 const jsonPath = '/var/www/html/observatorio/assets/bd/bd.json'
+const dotenvDir = '/var/www/src/observatorio/backend/.env';
+
+const result = require('dotenv').config({path: dotenvDir});
+const acervo = require('../models/acervo');
+const fs = require('fs');
 
 acervo.get(function(erro, result) {
 	if(erro) throw erro;
-	fs.writeFile(jsonPath, JSON.stringify(result).replace(/\:null/gi, "\:\"\""), (erro) =>{
+	fs.writeFile(jsonPath, JSON.stringify(result)
+		.replace(/\:null/gi, "\:\"\""), (erro) =>{
 		if(erro) throw err;
 		console.log('JSON criado com sucesso');
 		process.exit();
