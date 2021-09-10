8ef7dbf92e7ce83a1d04d991f0085c3a99d3ccb8
theRealCarneiro <gabriel.chaves.carneiro@gmail.com>
2021-09-09 22:48:06 -0300

    aba contato

---
+++ frontend/src/app/contato/contato.component.ts
@@ -0,0 +1,16 @@
+import { Component, OnInit } from '@angular/core';
+import { MatCardModule } from '@angular/material/card';
+
+@Component({
+	selector: 'app-contato',
+	templateUrl: './contato.component.html',
+	styleUrls: ['./contato.component.scss']
+})
+export class ContatoComponent implements OnInit {
+
+	constructor() { }
+
+	ngOnInit(): void {
+	}
+
+}
